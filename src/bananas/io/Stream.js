4bb9159a3cebc652764e04d9fbe2200e3cef7ac2
Daniel Friesen <github@nadir-seen-fire.com>
2009-07-25 23:38:50 -0700

    Start work on Stream object

--- src/bananas/io/Stream.js
+++ src/bananas/io/Stream.js
@@ -1,7 +1,54 @@
 
-function Stream() {
+function read(fnRead, len) {
+	if ( len === 0 )
+		throw new TypeError("Cannot read nothing from a stream");
 	
+	if ( !len ) {
+		var chunk, buf = new Buffer();
+		buf.encoding = this.encoding;
+		while( chunk = read.call(this, fnRead, Infinity) )
+			buf.append(chunk);
+		var data = buf.valueOf();
+	} else {
+		var data = fnRead(this.position, len);
+	}
+	this.position += data.length;
+	return data;
 }
+
+function wrapRead(fnRead) {
+	return function(len) {
+		try {
+			return read.call(this, fnRead, len);
+		} catch ( e if e === Stream.EOF ) {
+			return this.text ? "" : new Blob();
+		}
+	};
+}
+
+function Stream(obj) {
+	if (!(this instanceof Stream))
+		return new Stream(obj);
+	
+	if ( isFunction(obj.read) )
+		this.read = wrapRead(obj.read);
+	if ( isFunction(obj.write) )
+		this.write = wrapWrite(obj.write);
+	
+	this.__defineGetter__('encoding', function() {
+		return obj.encoding();
+	});
+	this.position = 0;
+}
+
+Stream.EOF = {eof:true};
+
+Stream.prototype.rewind = function() {
+	return this.position = 0;
+};
+Stream.prototype.__defineGetter__('text', function() {
+	this.encoding.lc !== 'binary';
+});
 
 Stream.prototype.yank = function(len) {
 	if ( !this.read ) // Error when read not implemented
@@ -25,4 +72,5 @@ Stream.prototype.yank = function(len) {
 	return buf.valueOf(); // valueOf returns String for text mode, and Blob for binary mode.
 };
 
+exports.Stream = Stream;
 
