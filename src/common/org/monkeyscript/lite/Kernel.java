8fd1eb978ec25826ac9b2d57707fceff0a6d0a0b
Daniel Friesen <github@nadir-seen-fire.com>
2009-07-27 18:01:28 -0700

    Bleh, this works... cept NativeError can't be used from outside mozilla.javascript

--- src/common/org/monkeyscript/lite/Kernel.java
+++ src/common/org/monkeyscript/lite/Kernel.java
@@ -4,21 +4,39 @@ import org.mozilla.javascript.*;
 
 public class Kernel {
 	
+	public static class ExtensibleError implements IdFunctionCall {
+		
+		protected static final int Id_new_CommonError = 1;
+		
+		public Object execIdCall(IdFunctionObject f, Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+			if (f.hasTag("Global")) {
+				int methodId = f.methodId();
+				if ( methodId == Id_new_CommonError ) {
+					return NativeError.make(cx, scope, f, args);
+				}
+			}
+			throw f.unknown();
+		}
+		
+	}
+	
 	public static void makeError(Context cx, Scriptable scope, boolean sealed, String name) {
-		/*Scriptable errorProto = ScriptRuntime.newObject(cx, scope, "Error", ScriptRuntime.emptyArgs);
+		Scriptable errorProto = ScriptRuntime.newObject(cx, scope, "Error", ScriptRuntime.emptyArgs);
 		errorProto.put("name", errorProto, name);
 		if (sealed) {
 			if (errorProto instanceof ScriptableObject) {
 				((ScriptableObject)errorProto).sealObject();
 			}
 		}
-		IdFunctionObject ctor = new IdFunctionObject(obj, "Global", Id_new_CommonError, name, 1, scope);
+		
+		IdFunctionCall xerr = new ExtensibleError();
+		IdFunctionObject ctor = new IdFunctionObject(xerr, "Global", ExtensibleError.Id_new_CommonError, name, 1, scope);
 		NativeError.make(cx, scope, f, args);
 		ctor.markAsConstructor(errorProto);
 		if (sealed) {
 			ctor.sealObject();
 		}
-		ctor.exportAsScopeProperty();*/
+		ctor.exportAsScopeProperty();
 	}
 	
 }
