92fbec8a4c42c3defd6f98b21f0aea24ce62aa8c
Daniel Friesen <github@nadir-seen-fire.com>
2009-07-29 23:46:36 -0700

    Use Kernel.die

--- src/banana/banana.js
+++ src/banana/banana.js
@@ -18,15 +18,10 @@ case 'collect':
 		Kernel.exit(help('collect'));
 	var registryFile = args.shift();
 	var bananaFiles = args;
-	if ( !registryFile ) {
-		print('Registry file not specified.');
-		Kernel.exit(1);
-	}
-	if ( !bananaFiles.length ) {
-		print("Can't build a registry of no bananas.");
-		Kernel.exit(1);
-	}
-	
+	if ( !registryFile )
+		Kernel.die('Registry file not specified.');
+	if ( !bananaFiles.length )
+		Kernel.die("Can't build a registry of no bananas.");
 	break;
 default:
 	print("Invalid action specified.");
