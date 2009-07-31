00b90abf130de502ddd4a59d2be0817bda77344c
Daniel Friesen <github@nadir-seen-fire.com>
2009-07-30 19:46:20 -0700

    Implement checks for coding lines inside of // and # comments. As well as support for # comments within the first two lines (ie: Shebang support)

--- src/common/org/monkeyscript/lite/ScriptReader.java
+++ src/common/org/monkeyscript/lite/ScriptReader.java
@@ -8,6 +8,8 @@ import java.io.InputStreamReader;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.regex.*;
+import java.util.HashMap;
 
 /**
  * Special ScriptReader class
@@ -24,6 +26,8 @@ public class ScriptReader extends Reader {
 	private static final byte[] BOM_UTF16LE = new byte[] { (byte)0xFF, (byte)0xFE };
 	private static final byte[] BOM_UTF16BE = new byte[] { (byte)0xFE, (byte)0xFF };
 	
+	private HashMap<Integer,Character> skipChars = new HashMap<Integer,Character>();
+	
 	public ScriptReader(InputStream is) throws IOException, UnsupportedEncodingException {
 		this.buf = new BufferedInputStream(is);
 		init();
@@ -38,22 +42,108 @@ public class ScriptReader extends Reader {
 	private void init() throws IOException, UnsupportedEncodingException {
 		BufferedInputStream is = this.buf;
 		String charset = "UTF-8"; // Default charsets (ToDo: Use global.monkeyscript.defaultEncoding instead)
-		is.mark(255); // Mark the start of the file before we look for charset info
+		String bomCharset = null;
 		
+		is.mark(3); // Mark the start of the file before we look for boms
+		int fstart = 0;
 		byte[] b = new byte[3];
 		is.read(b, 0, 3);
 		if ( b[0] == BOM_UTF16LE[0] && b[1] == BOM_UTF16LE[1] ) {
 			// UTF-16 LE BOM
-			charset = "UTF-16LE";
+			bomCharset = "UTF-16LE";
 			skip = 1; // BOM is one character
+			skipChars.put(0, ' ');
+			fstart = 2;
 		} else if ( b[0] == BOM_UTF16BE[0] && b[1] == BOM_UTF16BE[1] ) {
 			// UTF-16 BE BOM
-			charset = "UTF-16BE";
+			bomCharset = "UTF-16BE";
 			skip = 1; // BOM is one character
+			skipChars.put(0, ' ');
+			fstart = 2;
 		} else if ( Arrays.equals(b, BOM_UTF8) ) {
 			// UTF-8 BOM
-			charset = "UTF-8";
+			bomCharset = "UTF-8";
 			skip = 1; // Java treats the BOM as a single character, so just skip one
+			skipChars.put(0, ' ');
+			fstart = 3;
+		}
+		is.reset();
+		
+		if ( bomCharset != null ) {
+			charset = bomCharset;
+		}
+		
+		String charsetHint = bomCharset; // Try charset define by bom
+		if ( charsetHint == null ) charsetHint = this.charset; // Try user specified charset
+		if ( charsetHint == null ) charsetHint = "US-ASCII"; // Use US-ASCII as fallback
+		/*byte[] HASH = "#".getBytes(suggestedCharset);
+		byte[] SLASH = "/".getBytes(suggestedCharset);
+		byte[] CR = "\r".getBytes(suggestedCharset);
+		byte[] LF = "\n".getBytes(suggestedCharset);
+		
+		byte[] bb = new byte[HASH.length];
+		is.read(bb, fstart, HASH.length);
+		Arrays.equals(bb, HASH)
+		*/
+		
+		is.mark(256); // Mark the start of the file before we look for charset info
+		byte[] headBytes = new byte[256-fstart];
+		int len = is.read(headBytes, fstart, headBytes.length);
+		String head = new String(headBytes, charsetHint);
+		
+		String[] lines = new String[2];
+		int cr = head.indexOf("\r");
+		int lf = head.indexOf("\n");
+		int off = cr > lf ? cr : lf;
+		
+		if ( off == -1 ) {
+			lines[0] = head;
+		} else {
+			lines[0] = head.substring(0, off);
+			
+			if ( off == cr && lf == off+1 )
+				off++;
+			off++;
+			
+			cr = head.indexOf("\r", off);
+			lf = head.indexOf("\n", off);
+			int off2 = cr > lf ? cr : lf;
+			
+			if ( off2 == -1 ) {
+				lines[1] = head.substring(off);
+			} else {
+				lines[1] = head.substring(off, off2);
+			}
+		}
+		
+		Pattern re = Pattern.compile("coding[=:]\\s*([-_.A-Z0-9]+)", Pattern.CASE_INSENSITIVE);
+		for ( int l = 0; l != 2; l++ ) {
+			// ToDo: Support /* and make sure it doesn't cause bugs
+			if ( lines[l].startsWith("#") || lines[l].startsWith("//") ) {
+				Matcher m = re.matcher(lines[l]);
+				if ( m.find() ) {
+					charset = m.group(1);
+					break;
+				}
+			}
+		}
+		
+		// ToDo: This will break on the second line in some cases won't it? (like a multi-byte character somewhere in the mix)
+		if ( lines[0].startsWith("#") ) {
+			if ( lines[0].length() == 1 ) {
+				skipChars.put(skip, ' ');
+			} else {
+				skipChars.put(skip, '/');
+				skipChars.put(skip+1, '/');
+			}
+		}
+		if ( lines[1] != null && lines[1].startsWith("#") ) {
+			if ( lines[1].length() == 1 ) {
+				skipChars.put(skip+off, ' ');
+			} else {
+				skipChars.put(skip+off, '/');
+				skipChars.put(skip+off+1, '/');
+			}
 		}
 		
 		is.reset(); // Reset to the start of the file so the script may be read
@@ -75,10 +165,9 @@ public class ScriptReader extends Reader {
 	
 	public int read(char[] cbuf, int off, int len) throws IOException {
 		int readLength = cread.read(cbuf, off, len);
-		// This section of code replaces characters in the read buffer with spaces
-		// if they are below the skip threshold
-		for (int i = off; i < skip; i++) {
-			cbuf[i] = ' ';
+		for (int i = 0; i < readLength; i++) {
+			if ( skipChars.containsKey(i+off) )
+				cbuf[i] = skipChars.get(i+off);
 		}
 		return readLength;
 	}
