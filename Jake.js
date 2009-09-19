4f8045df06aa7bff849dc52cc0fbc29f26c8246e
Daniel Friesen <github@nadir-seen-fire.com>
2009-09-19 04:51:21 -0700

    Test extensions as well even though we haven't done any of them and some of them don't matter

--- Jake.js
+++ Jake.js
@@ -7,7 +7,15 @@ task('commonjs-test', function(options) {
 	Array.forEach(dir.listFiles(), function(testDir) {
 		if(testDir.isDirectory()) {
 			var t = String(testDir.getAbsolutePath());
-			print('//\n// Running test "'+testDir.getName()+'"\n//');
+			print('//\n// Running compliance test "'+testDir.getName()+'"\n//');
+			print(run("--rc", "javascript:require.paths.push('"+t+"');", t+"/program.js").output);
+		}
+	});
+	var dir = new java.io.File('commonjs-tests/extensions/');
+	Array.forEach(dir.listFiles(), function(testDir) {
+		if(testDir.isDirectory()) {
+			var t = String(testDir.getAbsolutePath());
+			print('//\n// Running extension test "'+testDir.getName()+'"\n//');
 			print(run("--rc", "javascript:require.paths.push('"+t+"');", t+"/program.js").output);
 		}
 	});
