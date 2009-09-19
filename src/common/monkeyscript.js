ac82145eb35e19017593a4f305a36903617239b0
Daniel Friesen <github@nadir-seen-fire.com>
2009-09-19 02:01:50 -0700

    Improve handling of command line arguments

--- src/common/monkeyscript.js
+++ src/common/monkeyscript.js
@@ -13,22 +13,7 @@ var monkeyscript = {
 (function(_native) {
 	delete global._native;
 	
-	// Arguments
-	var args = Array.slice(_native.arguments);
-	if ( args[0] === '-h' ) {
-		args.shift();
-		monkeyscript.hookName = args.shift();
-	} else {
-		monkeyscript.scriptName = args.shift();
-	}
-	
-	Object.seal(args);
-	monkeyscript.arguments = args;
-	global.arguments = Array.slice(args);
-	
-	global.Kernel = _native.Kernel;
-	Object.seal(Kernel);
-	
+	// RC Scripts
 	monkeyscript.rc.push(
 		Kernel.configDir + Kernel.os.fileSeparator + 'monkeyscriptrc.js',
 		Kernel.configDir + Kernel.os.fileSeparator + 'monkeyscriptrc'
@@ -42,6 +27,70 @@ var monkeyscript = {
 			monkeyscript.rc.push( basePath + Kernel.os.fileSeparator + baseName );
 		});
 	});
+	
+	// Arguments
+	var args = Array.slice(_native.arguments);
+	
+	function handleArg(name) {
+		switch(name) {
+		case 'l':
+		case 'lint':
+			// Lint a file
+			Kernel.die("MonkeyScript lint support has not been implemented yet");
+			break;
+		case 'i':
+		case 'a': // php uses -a sortof abnormally, support it for the hell of it
+		case 'interactive':
+		case 'repl':
+			// Run a hook
+			monkeyscript.hookName = "interactive";
+			handleArg.needScript = false;
+			break;
+		case 'h':
+		case 'hook':
+			// Run a hook
+			monkeyscript.hookName = args.shift();
+			handleArg.needScript = false;
+			break;
+		case 'no-rc':
+			// Empty out the list of rc files
+			monkeyscript.rc.clear();
+			break;
+		case 'rc':
+			// Add a new rc file to run
+			monkeyscript.rc.push(args.shift());
+			break;
+		default:
+			Kernel.die('Unknown argument "'+name+'".');
+		}
+	}
+	handleArg.needScript = true;
+	argsloop: for(var arg;args.length;) {
+		arg = args.shift();
+		if ( arg === "--" ) {
+			monkeyscript.scriptName = args.shift();
+			break;
+		} else if ( arg.length > 2 && arg.startsWith("--") ) {
+			handleArg(arg.substr(2));
+		} else if ( arg.length > 1 && arg.startsWith("-") ) {
+			arg.substr(1).split('').forEach(handleArg);
+		} else {
+			if ( handleArg.needScript )
+				monkeyscript.scriptName = arg;
+			else
+				args.unshift(arg);
+			break;
+		}
+	}
+	
+	Object.defineProperty(monkeyscript, "arguments", { value: args, writable: false, configurable: false });
+	global.arguments = Array.slice(args);
+	
+	global.Kernel = _native.Kernel;
+	
+	// Seal things
+	Object.seal(args);
+	Object.seal(Kernel);
 	Object.seal(monkeyscript.rc);
 	
 })(_native);
@@ -50,7 +99,12 @@ var monkeyscript = {
 var IOError = Kernel.newError("IOError");
 
 for each( let rc in monkeyscript.rc ) {
-	try { include.ifExists(rc); }
+	try {
+		if ( rc.startsWith('javascript:') )
+			eval(rc.substr('javascript:'.length));
+		else
+			include.ifExists(rc);
+	}
 	// Other errors will be printed, but still not affect running the script
 	catch( e ) { print(e); }
 }
@@ -61,10 +115,15 @@ if ( monkeyscript.hookName ) {
 		Kernel.die("The hook "+monkeyscript.hookName+" does not exist");
 }
 if ( monkeyscript.scriptName ) {
-	if ( monkeyscript.scriptName.startsWith('javascript:') )
+	if ( isFunction(monkeyscript.scriptName) )
+		monkeyscript.scriptName.apply(undefined, arguments);
+	else if ( monkeyscript.scriptName.startsWith('javascript:') )
 		eval(monkeyscript.scriptName.substr('javascript:'.length));
-	else
+	else {
+		if ( !Kernel.fs.exists(monkeyscript.scriptName) )
+			Kernel.die("The script "+monkeyscript.scriptName+" does not exist");
 		include(monkeyscript.scriptName);
+	}
 } else
 	Kernel.die("No script specified");
 
