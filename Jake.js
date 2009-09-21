0124b77ab5bb4f06ecc9a9be2da19522155bbb57
Daniel Friesen <github@nadir-seen-fire.com>
2009-09-20 22:07:18 -0700

    Switch test command from commonjs-test to test.commonjs and use a group for it.

--- Jake.js
+++ Jake.js
@@ -26,23 +26,25 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-task('commonjs-test', function(options) {
-	var {ProcessFunction} = banana('os.process');
-	var run = new ProcessFunction("monkeyscript");
-	var dir = new java.io.File('commonjs-tests/compliance/');
-	Array.forEach(dir.listFiles(), function(testDir) {
-		if(testDir.isDirectory()) {
-			var t = String(testDir.getAbsolutePath());
-			print('//\n// Running compliance test "'+testDir.getName()+'"\n//');
-			print(run("--rc", "javascript:require.paths.push('"+t+"');", t+"/program.js").output);
-		}
-	});
-	var dir = new java.io.File('commonjs-tests/extensions/');
-	Array.forEach(dir.listFiles(), function(testDir) {
-		if(testDir.isDirectory()) {
-			var t = String(testDir.getAbsolutePath());
-			print('//\n// Running extension test "'+testDir.getName()+'"\n//');
-			print(run("--rc", "javascript:require.paths.push('"+t+"');", t+"/program.js").output);
-		}
+group('test', function(options) {
+	task('commonjs', function(options) {
+		var {ProcessFunction} = banana('os.process');
+		var run = new ProcessFunction("monkeyscript");
+		var dir = new java.io.File('commonjs-tests/compliance/');
+		Array.forEach(dir.listFiles(), function(testDir) {
+			if(testDir.isDirectory()) {
+				var t = String(testDir.getAbsolutePath());
+				print('//\n// Running compliance test "'+testDir.getName()+'"\n//');
+				print(run("--rc", "javascript:require.paths.push('"+t+"');", t+"/program.js").output);
+			}
+		});
+		var dir = new java.io.File('commonjs-tests/extensions/');
+		Array.forEach(dir.listFiles(), function(testDir) {
+			if(testDir.isDirectory()) {
+				var t = String(testDir.getAbsolutePath());
+				print('//\n// Running extension test "'+testDir.getName()+'"\n//');
+				print(run("--rc", "javascript:require.paths.push('"+t+"');", t+"/program.js").output);
+			}
+		});
 	});
 });
